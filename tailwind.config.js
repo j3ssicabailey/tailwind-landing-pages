4de0c5acbcd3435b77f03e56914085e874ee75b3
Jessica Bailey <j3ssicabailey@gmail.com>
2025-01-21 19:12:08 +0100

    Adding themes, images and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#9B9A78",
+        primary: "#313131",
+        secondary: "#909090",
+      },
+      fontFamily: {
+        poppins: ['"Poppins"', 'Verdana', 'sans-serif'],
+        playfair: ['"Playfair Display"', "Times New Roman", "serif"],
+      },
+      spacing: {
+        'base': '16px', // Default base spacing
+        'section': '40px', // For larger sections
+        'content': '24px', // For padding within cards
+      },
+    },
   },
   plugins: [],
 }
